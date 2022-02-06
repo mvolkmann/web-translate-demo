87e49d09d7afbaf01113a1dd6e47d0e155053cc6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-06 07:50:35 -0600

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { getLanguageCode, i18n, setLanguage } from 'web-translate';
+
+	let languageCode = 'en';
+
+	changeLanguage('es');
+
+	async function changeLanguage(code) {
+		await setLanguage(code);
+		languageCode = code;
+	}
+</script>
+
+<h1>Welcome to web-translate</h1>
+
+{#key languageCode}
+	<p>Language is {languageCode}</p>
+	<p>Greeting is {i18n('hello')}.</p>
+{/key}
