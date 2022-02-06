01576e98889c6e4187afe71001b3420d0d0d1c3f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-06 08:09:16 -0600

    progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,30 @@
 <script>
-	import { getLanguageCode, i18n, setLanguage } from 'web-translate';
+  import {onMount} from 'svelte';
+  import {getSupportedLanguages, i18n, setLanguage} from 'web-translate';
 
-	let languageCode = 'en';
+  let languageCode = 'en';
+  let languages = {};
 
-	changeLanguage('es');
+  onMount(async () => {
+    languages = await getSupportedLanguages();
+  });
 
-	async function changeLanguage(code) {
-		await setLanguage(code);
-		languageCode = code;
-	}
+  async function changeLanguage(event) {
+    const code = event.target.value;
+    await setLanguage(code);
+    // Don't set this until after web-translate knows the new code.
+    languageCode = code;
+  }
 </script>
 
-<h1>Welcome to web-translate</h1>
+<h1>web-translate Demo</h1>
+
+<select on:change={changeLanguage}>
+  {#each Object.keys(languages).sort() as name}
+    <option value={languages[name]}>{name}</option>
+  {/each}
+</select>
 
 {#key languageCode}
-	<p>Language is {languageCode}</p>
-	<p>Greeting is {i18n('hello')}.</p>
+  <p>Greeting is {i18n('hello')}.</p>
 {/key}
